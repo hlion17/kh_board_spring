db50947fe1eed4b0928a2c13a5f5e0ee9f4a65a3
hlion17 <yourdad20626@gmail.com>
2022-04-13 07:55:39 +0900

    first commit

---
+++ src/main/java/web/service/impl/BoardServiceImpl.java
@@ -0,0 +1,10 @@
+package web.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import web.service.face.BoardService;
+
+@Service
+public class BoardServiceImpl implements BoardService {
+
+}
